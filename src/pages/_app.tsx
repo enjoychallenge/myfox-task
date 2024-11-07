23792db54cb0feaf5768283525978205c611dba2
Jiri Kozel <jirik@users.noreply.github.com>
2024-11-07 14:05:46 +0100

    Try apollo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import "@/styles/globals.css";
-import type {AppProps} from "next/app";
+import type { AppProps } from "next/app";
 import {Inter} from 'next/font/google'
+import { ApolloProvider } from "@apollo/client";
+import client from "@/lib/apolloClient";
 
 const inter = Inter({subsets: ['latin']})
 
 export default function App({Component, pageProps}: AppProps) {
   return (
-      <main className={inter.className}>
-        <Component {...pageProps} />
-      </main>
+      <ApolloProvider client={client}>
+        <div className={inter.className}>
+          <Component {...pageProps} />
+        </div>
+      </ApolloProvider>
   )
 }
